06bd6969b77ba8b9a9e1c0781191c77f131014a7
jiyoung <gyjeong021@gmail.com>
2025-08-13 02:47:20 +0900

    📝 Docs: 결제 등록 Swagger 명세서 작성

---
+++ src/main/java/glopal/backend/domain/transaction/service/TransactionCommandServiceImpl.java
@@ -0,0 +1,16 @@
+package glopal.backend.domain.transaction.service;
+
+import glopal.backend.domain.transaction.dto.request.TransactionRequest;
+import glopal.backend.domain.transaction.dto.response.TransactionResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionCommandServiceImpl implements TransactionCommandService {
+
+    @Override
+    public TransactionResponse.TransactionInfo createTransaction(TransactionRequest.CreateTransaction createTransaction) {
+        return null;
+    }
+}
